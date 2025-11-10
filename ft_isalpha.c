0a7ddff354fd5153539b193724ba4a90537d1b2a
Elhadj Amadou Barry <elbarry@z1r10p1.42lyon.fr>
2025-11-10 17:00:37 +0100

    is alpha function

---
+++ ft_isalpha.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elbarry <elbarry@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 14:49:24 by elbarry           #+#    #+#             */
+/*   Updated: 2025/11/10 15:36:35 by elbarry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isalpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+#include <stdio.h>
+int	main (void)
+{
+	printf("%d", ft_isalpha("elhadj8"));
+	return (0);
+}
